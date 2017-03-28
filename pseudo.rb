fb636d2f8641320f6413ace783897623ba1d7bab
quantumduck <quantum.duck@hotmail.com>
2017-03-28 09:41:17 -0400

    pseudo code

---
+++ pseudo.rb
@@ -0,0 +1,12 @@
+# Let's do the Bitmaker HQ thing!
+
+# for numbers 1 to 100
+  # if number is multiple of 3
+    # print Bitmaker
+  # if number is multiple of 5
+    # print HQ
+  # if nothng has been printed yet,
+    # print number
+  # pring newline
+
+  
