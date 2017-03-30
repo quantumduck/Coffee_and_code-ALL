a8eef00b31c1a284cb3bb20d198d064a13971059
quantumduck <quantum.duck@hotmail.com>
2017-03-30 09:35:03 -0400

    added other methods

--- pizza.rb
+++ pizza.rb
@@ -4,7 +4,7 @@ class Pizza
     @toppings = toppings
     @cooked = false
     @cut = false
-    @slices = 0
+    @slices = 1
     @burnt = false
   end
 
@@ -16,19 +16,43 @@ class Pizza
       @burnt = true
       puts "You burnt your pizza :("
     end
+    @cooked
   end
 
   def cut(numslices = 10)
     if (numslices.to_i > 0)
       if @cooked
         @slices = numslices.to_i
+        @cut = true
+        puts "You cut your pizza into #{@slices} slices."
       else
-        puts "Please cut your pizza first."
+        puts "Please bake your pizza first."
       end
-      puts "You cut your pizza into #{@slices} slices."
     else
-      puts "You fail. Pick an "
+      puts "You fail. Pick a positive number of slices."
     end
+    @slices
+  end
 
+  def eat(numslices = 1)
+    if !@cooked
+      puts "Please bake your pizza first."
+    elsif @burnt
+      puts "Your pizza is burnt and inedible."
+    else
+      if (@slices == 0)
+        puts "You have no pizza left."
+      elsif (!@cut)
+        @slices = 0
+        puts "You ate the whole pizza, you pig."
+      elsif (@slices ==1)
+        @slices = 0
+        puts "You finished your pizza."
+      else
+        @slices -= 1
+        puts "You ate a slice. You have #{@slices} left."
+      end
+    end
+  end
 
 end
