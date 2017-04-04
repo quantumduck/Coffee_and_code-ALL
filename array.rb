85bdb05add3d81ce3fd96b85b321d81cc71f746a
quantumduck <quantum.duck@hotmail.com>
2017-04-04 09:15:40 -0400

    reversals

--- array.rb
+++ array.rb
@@ -4,4 +4,11 @@ def mk_array(n)
   array
 end
 
+def screw_up(array)
+  array = array.reverse
+  array = array.map { |s| s.reverse }
+end
+
 puts mk_array(8)
+
+puts screw_up(["first", "second", "third", "fourth", "fifth", "sixth"])
