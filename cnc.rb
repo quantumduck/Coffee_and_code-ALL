2bd9beb9994872e47383a72eec2230210c987e8a
quantumduck <quantum.duck@hotmail.com>
2017-04-11 10:05:08 -0400

    finished

--- cnc.rb
+++ cnc.rb
@@ -62,18 +62,33 @@
     case number
     when Integer
       unless number >= 0
-        return "Error: #{number} is negative."
+        puts "Error: #{number} is negative."
+        return nil
+      end
+      number = number.to_s
     when String
       number.length.times do |i|
         unless number[i] == '0'
           if number[i].to_i <= 0
-            return "Error: #{number[i]} is not a decimal digit."
+            puts "Error: #{number[i]} is not a decimal digit."
+            return nil
           end
         end
       end
-      
     else
-      puts "else"
+      puts "Error: #{number} is not a string or integer."
+      return nil
+    end
+    left_sum = 0
+    right_sum = 0
+    (number.length/2).times do |i|
+      left_sum += number[i].to_i
+      right_sum += number[number.length - i - 1].to_i
+    end
+    if left_sum == right_sum
+      return true
+    else
+      return false
     end
   end
 
@@ -90,3 +105,19 @@ puts 'valid_phone_number("(098) 123 4567") # => returns false'
 puts valid_phone_number("(123) 456-7890") # => returns true
 puts valid_phone_number("(1111)555 2345") # => returns false
 puts valid_phone_number("(098) 123 4567") # => returns false
+
+
+puts
+
+puts "003111 # 3 = 1 + 1 + 1"
+puts "813372 # 8 + 1 + 3 = 3 + 7 + 2"
+puts "17935 # 1 + 7 = 3 + 5"
+puts "56328116"
+puts "strawberry"
+
+puts
+puts luck_check("003111")
+puts luck_check(813372)
+puts luck_check(17935)
+puts luck_check(56328116)
+puts luck_check("strawberry")
