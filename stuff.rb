7ea380d0263de9f82ad33fadc68b86122d2ca061
quantumduck <quantum.duck@hotmail.com>
2017-04-13 09:37:18 -0400

    done

--- stuff.rb
+++ stuff.rb
@@ -14,6 +14,22 @@ animals = [['dogs', 4], ['cats', 3], ['dogs', 7]]
 
 puts array_to_hash(animals)
 
-def find_leap_years(start, end)
-
+def find_leap_years(start, ending)
+  leap_years = (start..ending).to_a
+  leap_years.delete_if { |y| (y % 4 != 0) || ((y % 100 == 0) && (y % 400 != 0)) }
+  leap_years
 end
+
+puts find_leap_years(2000, 2012).to_s
+
+def find_longest(arr)
+  longest = arr.first
+  arr.each do |entry|
+    longest = entry if entry.length > longest.length
+  end
+  longest
+end
+
+colours = %w(red blue green yellow chartreuse orange periwinkle tomato cobalt)
+
+puts find_longest(colours)
