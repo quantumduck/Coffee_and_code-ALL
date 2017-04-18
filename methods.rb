d2d23cc3fb782470b62b478de73c943fdd8ead99
quantumduck <quantum.duck@hotmail.com>
2017-04-18 09:17:40 -0400

    third exercise

--- methods.rb
+++ methods.rb
@@ -26,3 +26,13 @@ puts
 2000.times do |num|
   puts num if is_narcissistic(num)
 end
+
+def gigasecond(year: year, month: month, day: day)
+  time = Time.mktime(year, month, day)
+  time += 1_000_000_000
+  {year: time.year, month: time.month, day: time.day}
+end
+
+puts
+puts "gigasecond(year: 2017, month: 4, day: 18) = "
+puts gigasecond(year: 2017, month: 4, day: 18)
