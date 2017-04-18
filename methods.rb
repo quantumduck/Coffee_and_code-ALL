f7f6d8336e0f0190f797223bc92c2a06604901b6
quantumduck <quantum.duck@hotmail.com>
2017-04-18 09:05:56 -0400

    2nd exercise

--- methods.rb
+++ methods.rb
@@ -11,3 +11,18 @@ end
 song = [["row", 3], ["your", 1], ["boat", 1]]
 puts "song = " + song.to_s
 puts expand(song).to_s
+
+def is_narcissistic(num)
+  digits = num.to_s.split('').map { |d| d.to_i }
+  # puts digits
+  num_digits = digits.length
+  dsum = 0
+  digits.each { |d| dsum += d**num_digits }
+  # puts dsum
+  dsum == num
+end
+
+puts
+2000.times do |num|
+  puts num if is_narcissistic(num)
+end
