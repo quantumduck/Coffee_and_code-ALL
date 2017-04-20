b9bbb222832f0b613d225aec4125c8a0b6354f4f
quantumduck <quantum.duck@hotmail.com>
2017-04-20 09:48:21 -0400

    stuff

---
+++ math.rb
@@ -0,0 +1,42 @@
+def prime(max_val)
+  indexes = (max_val - 1).times.to_a
+  primes = indexes.map { |i| i + 2 }
+  index = 0
+  while index < primes.length
+    primes.delete_if { |p| (p > primes[index]) && (p % primes[index] == 0) }
+    index += 1
+  end
+  primes
+end
+
+
+puts prime(100).to_s
+
+def mult_table(n)
+  mult_table = []
+  n.times do |row|
+    mult_table << []
+    n.times { |col| mult_table[row] << (row + 1) * (col + 1) }
+  end
+  mult_table
+end
+
+puts
+puts mult_table(6).to_s
+
+
+def get_squares(array)
+  array = array.to_a
+  max_num = array.max
+  n = 1
+  all_squares = []
+  while n**2 <= max_num
+    all_squares << n**2
+    n += 1
+  end
+  all_squares.delete_if { |sq| !array.include?(sq) }
+end
+puts
+puts get_squares(1..16).to_s # => [1, 4, 9, 16]
+puts get_squares(1..100).to_s # => [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+puts get_squares([4, 1, 16, 1]).to_s
