ff9bfc825da7a0adec33453d99987737ae451238
quantumduck <quantum.duck@hotmail.com>
2017-04-25 09:48:20 -0400

    refactored to not use .each

--- regex.rb
+++ regex.rb
@@ -1,8 +1,6 @@
 def find_png(file_name_list)
-  matches = []
-  file_name_list.each do |file|
-    matches << file if file.match(/\.png$/)
-  end
+  matches = file_name_list
+  matches.delete_if { |file| file.match(/\.png$/) }
   matches
 end
 
@@ -10,10 +8,8 @@ puts "find_png(['file1.jpg', 'file2.png']).to_s"
 puts find_png(["file1.jpg", "file2.png"]).to_s
 
 def find_three_letters(list)
-  matches = []
-  list.each do |string|
-    matches << string if string.match(/^.{3}$/)
-  end
+  matches = list
+  matches.delete_if { |string| string.match(/^.{3}$/) }
   matches
 end
 
@@ -21,10 +17,8 @@ puts "find_three_letters(['cat', 'dog', 'rhinoceros']).to_s"
 puts find_three_letters(['cat', 'dog', 'rhinoceros']).to_s
 
 def find_non_bs(list)
-  matches = []
-  list.each do |string|
-    matches << string if string.match(/^[^B]/)
-  end
+  matches = list
+  matches.delete_if { |string| string.match(/^[^B]/) }
   matches
 end
 
