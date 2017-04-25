d48d952b4d3df820985cf309a636e277322ea0ac
quantumduck <quantum.duck@hotmail.com>
2017-04-25 09:54:18 -0400

    fixed 'not' errors. Last method still not working

--- regex.rb
+++ regex.rb
@@ -1,6 +1,6 @@
 def find_png(file_name_list)
   matches = file_name_list
-  matches.delete_if { |file| file.match(/\.png$/) }
+  matches.delete_if { |file| !file.match(/\.png$/) }
   matches
 end
 
@@ -9,18 +9,27 @@ puts find_png(["file1.jpg", "file2.png"]).to_s
 
 def find_three_letters(list)
   matches = list
-  matches.delete_if { |string| string.match(/^.{3}$/) }
+  matches.delete_if { |string| !string.match(/^.{3}$/) }
   matches
 end
 
 puts "find_three_letters(['cat', 'dog', 'rhinoceros']).to_s"
 puts find_three_letters(['cat', 'dog', 'rhinoceros']).to_s
 
-def find_non_bs(list)
+def find_non_intial_bs(list)
   matches = list
-  matches.delete_if { |string| string.match(/^[^B]/) }
+  matches.delete_if { |string| string.match(/^B/) }
   matches
 end
 
-puts "find_non_bs(['Ben', 'Edwin', 'Lauren']).to_s"
-puts find_non_bs(['Ben', 'Edwin', 'Lauren']).to_s
+puts "find_non_intial_bs(['Ben', 'Edwin', 'Lauren']).to_s"
+puts find_non_intial_bs(['Ben', 'Edwin', 'Lauren']).to_s
+
+def find_no_bs_at_all(list)
+  matches = list
+  matches.delete_if { |string| string.match(/[Bb]+/) }
+  matches
+end
+
+puts "find_non_intial_bs(['Ben', 'Edwin', 'Lauren', 'Ruby']).to_s"
+puts find_non_intial_bs(['Ben', 'Edwin', 'Lauren', 'Ruby']).to_s
