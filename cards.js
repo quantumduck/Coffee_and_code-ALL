a9c7809698ecf857091357d6569f4becfaf02305
quantumduck <quantum.duck@hotmail.com>
2017-04-27 09:30:29 -0400

    working

--- cards.js
+++ cards.js
@@ -1,9 +1,11 @@
 var alice =  {
+  name: 'Alice',
   points: 0,
   deck: []
 }
 
 var bob = {
+  name: 'Bob',
   points: 0,
   deck: []
 }
@@ -20,15 +22,47 @@ function compare(c1, c2) {
   if (c1 === c2) {
     return '=';
   }
-  if !(c1 > 0) {
-    c1 = rank['c1'];
+  if (!(c1 > 0)) {
+    c1 = rank[c1];
   }
-  if !(c2 > 0) {
-    c2 = rank['c2'];
+  if (!(c2 > 0)) {
+    c2 = rank[c2];
   }
   if (c1 > c2) {
     return '>';
   } else {
-    return '>';
+    return '<';
   }
 }
+
+
+function winner(player1, player2) {
+  var num_cards = player1.deck.length;
+  if (player2.deck.length != num_cards) {
+    console.log("Players have different numbers of cards.");
+    return;
+  }
+  for (var i = 0; i < num_cards; i++) {
+    switch (compare(player1.deck[i], player2.deck[i])) {
+      case '>':
+        player1.points++;
+        break;
+      case '<':
+        player2.points++;
+        break;
+    }
+    console.log(player1.deck[i] + player2.deck[i] + compare(player1.deck[i], player2.deck[i]));
+  }
+  if (player1.points === player2.points) {
+    console.log(player1.name + ' and ' + player2.name + ' tie ' + player1.points + ' to ' + player2.points);
+  } else if (player1.points > player2.points) {
+    console.log(player1.name + ' wins ' + player1.points + ' to ' + player2.points);
+  } else {
+    console.log(player2.name + ' wins ' + player2.points + ' to ' + player1.points);
+  }
+}
+
+alice.deck = ['A', '7', '8'];
+bob.deck = ['K', '5', '9'];
+
+winner(alice, bob);
